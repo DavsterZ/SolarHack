deed6629a82f4c79c1529e1a30569c481647506e
DavsterZ <davsterz@gmail.com>
2025-12-05 09:25:49 +0100

    LLamando a las task de adc e ina he imprimiendo datos necesarios

--- main/main.c
+++ main/main.c
@@ -1,20 +1,15 @@
 #include "battery.h"
 #include "ina.h"
+#include "adc.h"
 
 #include "esp_log.h"
 #include "esp_err.h"
 
 #include "freertos/FreeRTOS.h"
-#include "freertos/projdefs.h"
 #include "freertos/task.h"
 
 
-#define INA_PANEL_ADDR   0x40
-#define INA_BAT_ADDR     0x41
 #define BAT_CAPACITY_AH  2.6f
-#define INA_RSHUNT_OHMS  0.1f	// Cambiar
-#define INA_PANEL_IMAX_A 1.0f
-#define INA_BAT_IMAX_A   1.0f
 #define LOOP_PERIOD_S    1.0f
 
 
@@ -22,20 +17,15 @@ static const char *TAG = "MAIN";
 
 void app_main(void)
 {	
-	ina219_t ina_entrada; 	// Hacia Panel
-	ina219_t ina_salida;	// Hacia bateria
+	xTaskCreate(ina_task, "ina_task", 4096, NULL, 5, NULL);
+    xTaskCreate(adc_task, "adc_task", 4096, NULL, 5, NULL);
 
-
-	// Direccion entrada: 0x40
-	// Direcion salida: 0x41
-	// Iniciar INA
-	ESP_ERROR_CHECK(ina219_init(&ina_entrada, INA_PANEL_ADDR, INA_RSHUNT_OHMS, INA_PANEL_IMAX_A));
-	ESP_ERROR_CHECK(ina219_init(&ina_salida, INA_BAT_ADDR, INA_RSHUNT_OHMS, INA_BAT_IMAX_A));
+	vTaskDelay(pdMS_TO_TICKS(1500));
 
 	float soc = 50.0f;
-    float v_bat_init = 0.0f;
+    float v_bat_init = g_ina219_data[INA219_DEVICE_BATTERY].bus_voltage_V;
 
-    if (ina219_read_bus_voltage(&ina_salida, &v_bat_init) == ESP_OK) {
+    if (v_bat_init > 1.0f) {
         soc = battery_porcent_from_voltage(v_bat_init);
         ESP_LOGI(TAG, "SoC inicial estimado: Vbat=%.3f V -> %.1f%%",
                  v_bat_init, soc);
@@ -44,39 +34,39 @@ void app_main(void)
     }
 
 
-    while (true) {
-        float v_entrada = 0.0f, i_entrada = 0.0f, w_entrada = 0.0f;
-		float v_salida = 0.0f, i_salida = 0.0f, w_salida = 0.0f;
+    while (1) {
+    	float v_entrada = g_ina219_data[INA219_DEVICE_PANEL].bus_voltage_V;
+        float i_entrada = g_ina219_data[INA219_DEVICE_PANEL].current_A;
+        float w_entrada = g_ina219_data[INA219_DEVICE_PANEL].power_W;
 
+        float v_salida  = g_ina219_data[INA219_DEVICE_BATTERY].bus_voltage_V;
+        float i_salida  = g_ina219_data[INA219_DEVICE_BATTERY].current_A;
+        float w_salida  = g_ina219_data[INA219_DEVICE_BATTERY].power_W;
 		
-		if (ina219_read_bus_voltage(&ina_entrada ,&v_entrada) == ESP_OK && 
-			ina219_read_current(&ina_entrada ,&i_entrada) == ESP_OK &&  
-			ina219_read_power(&ina_entrada ,&w_entrada) == ESP_OK && 
-			ina219_read_bus_voltage(&ina_salida ,&v_salida) == ESP_OK &&
-			ina219_read_current(&ina_salida ,&i_salida) == ESP_OK && 
-			ina219_read_power(&ina_salida ,&w_salida) == ESP_OK)
-		{	
 
-			// OJO: aquí asumimos que i_bat > 0 significa que la batería
-            // se está DESCARGANDO. Si en tu montaje es al revés,
-            // pon: i_bat = -i_bat;
-            // para que el SoC suba cuando se cargue y baje cuando se descargue.
-			
-			soc = battery_soc_update(
-                soc,
-                v_salida,
-                i_salida,
-                LOOP_PERIOD_S,
-                BAT_CAPACITY_AH
-            );
-			
-			ESP_LOGI(TAG,
-                     "IN:  V=%.3f V  I=%.3f A  P=%.3f W | OUT: V=%.3f V  I=%.3f A  P=%.3f W, SoC=%.1f%%",
-                     v_entrada, i_entrada, w_entrada, v_salida, i_salida, w_salida, soc);
-		}
-		else
-			ESP_LOGE(TAG, "Error leyendo algunos de los INA219");
+		// OJO: aquí asumimos que i_bat > 0 significa que la batería
+        // se está DESCARGANDO. Si en tu montaje es al revés,
+        // pon: i_bat = -i_bat;
+        // para que el SoC suba cuando se cargue y baje cuando se descargue.
+		
+		soc = battery_soc_update(
+            soc,
+            v_salida,
+            i_salida,
+            LOOP_PERIOD_S,
+            BAT_CAPACITY_AH
+        );
+		
+		ESP_LOGI(TAG,
+                 "IN:  V=%.3f V  I=%.3f A  P=%.3f W | OUT: V=%.3f V  I=%.3f A  P=%.3f W, SoC=%.1f%%",
+                 v_entrada, i_entrada, w_entrada, v_salida, i_salida, w_salida, soc);
 
+		ESP_LOGI(TAG, "LDR1: %.2f kOhm", g_ldr_data[0].resistance_kohm);
+		ESP_LOGI(TAG, "LDR1: %.2f kOhm", g_ldr_data[1].resistance_kohm);
+		ESP_LOGI(TAG, "LDR1: %.2f kOhm", g_ldr_data[2].resistance_kohm);
+		ESP_LOGI(TAG, "LDR1: %.2f kOhm", g_ldr_data[3].resistance_kohm);
+	
+	
 		vTaskDelay(pdMS_TO_TICKS((int)(LOOP_PERIOD_S * 1000)));
     }
 }
