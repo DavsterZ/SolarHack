f9820e33ca0dc89df8d54517e644933d631048d1
DavsterZ <davsterz@gmail.com>
2025-12-07 10:58:23 +0100

    Refactorizacion

--- main/nvs_managment.h
+++ main/nvs_managment.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <esp_log.h>
-#include "nvs.h"
 
 void save_wifi_credentials(const char* ssid, const char* pass);
 bool load_wifi_credentials(char *ssid, size_t ssid_size, char* pass, size_t pass_size);
